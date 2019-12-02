3f25040064590c70a9b6b2f83c9c5cbb68928dfa
wolflop <35335371+wolflop@users.noreply.github.com>
2019-12-02 17:48:16 +0800

    Update 三数之和等于0.py

--- 三数之和等于0.py
+++ 三数之和等于0.py
@@ -1,1 +1,21 @@
 # -*- coding:utf-8 -*-
+
+class solution:
+    def __init__(self, nums):
+        self.nums = nums
+    def threeSum(self):
+        output=[]
+        '''
+        算法复杂度最高，益理解。
+        依次循环，从而选出不同的值，但是相加等于0的。
+        '''
+        for countA in range(len(self.nums)):
+            # print(countA)
+            for countB in range(countA, len(self.nums)):
+                for countC in range(countB, len(self.nums)):
+                    if countA != countB and countA != countC and countB != countC:
+                        if self.nums[countC] + self.nums[countA] + self.nums[countB] == 0:
+                            output.append([self.nums[countA], self.nums[countB], self.nums[countC]])
+        print(output)
+        return output
+    
